f28afe89d0baa18171adf0b56139b4f664a89967
Kevin Osei <knokwaku@gmail.com>
2024-04-05 21:33:46 +0000

    initial layout setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const app = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+  </main>
+);
+
+export default App;
