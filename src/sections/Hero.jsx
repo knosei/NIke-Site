010968b4f39a6252a54ee43b37ee3f4ddaaf161d
Kevin Osei <knokwaku@gmail.com>
2024-04-15 22:04:39 +0000

    stats area for hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from '../components/Button';
 import { arrowRight } from '../assets/icons'
+import { statistics } from '../constants';
 
 const Hero = () => {
   return (
@@ -18,6 +19,15 @@ const Hero = () => {
         <p>Discover stylish Nike arrivals, quality comfort, and innovation
            for your active life.</p>
         <Button label="Shop now" iconURL={arrowRight} />
+        <div className='flex justify-start items-start flex-wrap w-full mt-20 
+        gap-16'>
+          {statistics.map((stat, idex) => (
+            <div>
+              <p>{stat.value}</p>
+              <p>{stat.label}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   )
