8510c3d01ab360b9926687cbab923ffb8e593ce5
Kevin Osei <knokwaku@gmail.com>
2024-04-22 21:41:37 +0000

    hero section header text

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,14 +10,15 @@ const Hero = () => {
       <div className='relative xl:w-2/5 flex flex-col justify-center 
       items-start w-full max-xl:padding-x pt-28'>
         <p className='text-xl font-montserrat text-coral-red'>Our Summer Collection</p>
-        <h1>
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading[82] 
+        font-bold'>
           <span>The New Arrival</span>
           <br />
           <span>Nike</span>
           Shoes
         </h1>
         <p>Discover stylish Nike arrivals, quality comfort, and innovation
-           for your active life.</p>
+          for your active life.</p>
         <Button label="Shop now" iconURL={arrowRight} />
         <div className='flex justify-start items-start flex-wrap w-full mt-20 
         gap-16'>
