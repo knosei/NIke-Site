21654d296de9095ad24b69cca9713695813980ac
Kevin Osei <knokwaku@gmail.com>
2024-04-23 21:26:43 +0000

    text styling

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -12,12 +12,14 @@ const Hero = () => {
         <p className='text-xl font-montserrat text-coral-red'>Our Summer Collection</p>
         <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading[82] 
         font-bold'>
-          <span>The New Arrival</span>
+          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
+            The New Arrival</span>
           <br />
-          <span>Nike</span>
-          Shoes
+          <span className='text-coral-red inline-block mt-3'>
+            Nike</span> Shoes
         </h1>
-        <p>Discover stylish Nike arrivals, quality comfort, and innovation
+        <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
+          Discover stylish Nike arrivals, quality comfort, and innovation
           for your active life.</p>
         <Button label="Shop now" iconURL={arrowRight} />
         <div className='flex justify-start items-start flex-wrap w-full mt-20 
