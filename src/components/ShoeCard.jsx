f2c3f2054e152e9125423e3812b00b9e452fddca
Kevin Osei <knokwaku@gmail.com>
2024-04-28 21:55:25 +0000

    imgURL not working

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
-const ShoeCard = () => {
+const ShoeCard = ({imgURL, changeBigShoeImage, bigShoeImg}) => {
+
+const handleClick = () => {
+    if(bigShoeImg !== imgURL.bigShoe){
+        changeBigShoeImage(imgURL.bigShoe)
+    }
+}
+
   return (
-    <div>ShoeCard</div>
+    <div className={`border-2 rounded-xl 
+    ${bigShoeImg === imgURL ? 'border-coral-red' : 'border-transparent'} 
+    cursor-pointer max-sm:flex-1`}
+    onClick={handleClick}>
+        <div>
+            <img src={imgURL.thumbnail} alt="shoe collection" />
+        </div>
+    </div>
   )
 }
 
 export default ShoeCard
+
+
